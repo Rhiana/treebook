4b2b523ff8bbf2a3c43b2c2bbbebb36d6fb1de79
Rhiana Heath <rhiana@heath.cc>
2013-11-13 14:07:40 +1100

    Fixed bug with user profile validation

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -17,7 +17,7 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should render the new page when logged in" do 
+  test "should render the new page when logged in" do
     sign_in users(:rhiana)
     get :new
     assert_response :success
@@ -31,9 +31,9 @@ class StatusesControllerTest < ActionController::TestCase
 
   test "should create status when logged in" do
     sign_in users(:rhiana)
-
+    post :create, status: { content: "Hello" }
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
+      post :create, status: { content: @status.content }
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -44,29 +44,31 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should get edit when logged in" do
     sign_in users(:rhiana)
-
     get :edit, id: @status
     assert_response :success
   end
 
   test "should redirect status update when not logged in" do
-    put :update, id: @status, status: { content: @status.content}
+    put :update, id: @status, status: { content: @status.content }
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
 
-
   test "should update status when logged in" do
     sign_in users(:rhiana)
-    put :update, id: @status, status: { content: @status.content}
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
-  test "should destroy status when logged in" do
-    sign_in users(:rhiana)
-
+  test "should destroy status" do
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
